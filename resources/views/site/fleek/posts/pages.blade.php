3ab6ab3a7677fa2cfcd51f4ec19ac3ea5ac4dd24
Confraria Web <confrariaweb@gmail.com>
2017-03-29 16:22:50 -0300

    Update gitignore

--- resources/views/site/fleek/posts/pages.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('site.fleek.layouts.default')
-
-@section('content')
-
-@include('site.fleek.partials.header-internas', ['title'=>$post->title,'h1'=>$post->title,'h2'=>$post->content])
-
-@include('site.fleek.partials.pages.'.$post->slug)
-
-@endsection
