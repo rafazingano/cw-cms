4f09fe4f29a52360d84afa88a430b9451c87e640
Confraria Web <confrariaweb@gmail.com>
2017-03-29 13:25:54 -0300

    Update lang leads

--- app/Mail/Contact.php
+++ app/Mail/Contact.php
@@ -9,18 +9,19 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Lead;
 use App\LeadRule;
 
-class Contact extends Mailable
-{
-    use Queueable, SerializesModels;
+class Contact extends Mailable {
+
+    use Queueable,
+        SerializesModels;
 
     public $lead;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(Lead $lead)
-    {
+    public function __construct(Lead $lead) {
         $this->lead = $lead;
     }
 
@@ -29,15 +30,16 @@ class Contact extends Mailable
      *
      * @return $this
      */
-    public function build()
-    {
+    public function build() {
         $this->from('admin@agenciafleek.com.br', 'Agencia Fleek');
-        if(isset($this->lead->lead_rule)){
+        if (isset($this->lead->lead_rule)) {
             $lr = LeadRule::where(['slug' => $this->lead->lead_rule])->first();
             $this->subject($lr->title);
-            $tos = (array) $lr->users;
-            $this->to($tos);
+            foreach ($lr->users as $u) {
+                $this->to($u->email);
+            }
         }
         return $this->view('mails.contact');
     }
+
 }
